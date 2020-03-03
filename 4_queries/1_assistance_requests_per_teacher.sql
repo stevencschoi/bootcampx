8456db5b0600a481f48df481e421dd313e5591a7
Steven Choi <stevencschoi87@gmail.com>
2020-03-03 17:51:07 +0000

    added first 7 solutions to queries

---
+++ 4_queries/1_assistance_requests_per_teacher.sql
@@ -0,0 +1,6 @@
+SELECT COUNT(assistance_requests.*) AS total_assistances, teachers.name
+FROM assistance_requests
+JOIN teachers ON teacher_id = teachers.id
+WHERE name = 'Waylon Boehm'
+GROUP BY name;
+-- HAVING name = 'Waylon Boehm';
---
+++ 4_queries/2_total_student_assistance_requests.sql
@@ -0,0 +1,4 @@
+SELECT count(assistance_requests.*) AS total_assistances, name FROM assistance_requests
+JOIN students ON students.id = student_id
+WHERE name = 'Elliot Dickinson'
+GROUP BY students.name;
